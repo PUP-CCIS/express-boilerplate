f0edb752aac701f0debe39d26bfd94fdcfd74eed
Micah Andrew F. Bule <micah.bule@gmail.com>
2017-08-17 02:04:10 +0800

    Added routes for inserting and updating certain tasks

--- app/modules/api/todos/api.js
+++ app/modules/api/todos/api.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 var db = require('../../../lib/database')();
 
+// /api/todos
 router.get('/', (req, res) => {
     db.query('SELECT * FROM todos', (err, results, fields) => {
         if (err) return res.status(400).send({ error: err });
@@ -8,10 +9,27 @@ router.get('/', (req, res) => {
     });
 });
 
+// /api/todos
 router.post('/', (req, res) => {
     db.query('INSERT INTO todos (`title`, `description`, `done`) VALUES (?, ?, false)', [req.body.title, req.body.description], (err, results, fields) => {
         if (err) return res.status(400).send({ error: err.toString() });
         res.status(200).send({ message: 'Successfully added todo!' });
+    });
+});
+
+// /api/todos/:id
+router.get('/:id', (req, res) => {
+    db.query('SELECT * FROM todos WHERE id=?', [req.params.id], (err, results, fields) => {
+        if (err) return res.status(400).send({ error: err.toString() });
+        res.status(200).send(results[0]);
+    });
+});
+
+// /api/todos/:id
+router.put('/:id', (req, res) => {
+    db.query('UPDATE todos SET title=?, description=? WHERE id=?', [req.body.title, req.body.description, req.params.id], (err, results, fields) => {
+        if (err) return res.status(400).send({ error: err.toString() });
+        res.status(200).send({ message: 'Successfully updated item.' });
     });
 });
 
